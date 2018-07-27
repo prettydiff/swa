08ef3d9e91e207c6647bf82ad01fc3e3189ade83
prettydiff <info@prettydiff.com>
2018-07-27 15:38:07 -0400

    testing stuff

--- webserver.ts
+++ webserver.ts
@@ -5,16 +5,18 @@
             https: require("https")
         },
         insecure = node.http.createServer(function insecure_create(request, response):void {
-            console.log(request);
+            console.log("insecure");console.log(request);
         }),
         secure = node.https.createServer(function secure_create(request, response):void {
-            
+            console.log("secure");console.log(request);
         }),
         serverError = function node_apps_server_serverError(error):void {
             console.log(`${error.Error}`);
             process.exit(1);
         };
     insecure.on("error", serverError);
-    insecure.listen(80);
+    insecure.listen(8000);
+    secure.on("error", serverError);
+    secure.listen(4430);
     console.log("http server is up.");
 }());
